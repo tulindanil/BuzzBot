8f3da72644fc9ebdcff6463d641c17aca43db180
tulindanil <tulindanil@gmail.com>
2016-05-13 21:27:24 +0300

    bug fix.

--- utils/encoder.py
+++ utils/encoder.py
@@ -3,12 +3,12 @@ import json
 class Encoder(dict):
 
     def __init__(self, path):
-        dict.__init__()
+        super(Encoder, self).__init__()
         with open(path) as descriptor:
             self.content = json.load(descriptor)
 
     def __getitem__(self, index):
-        return self.content.get(index, index)
+        return self.content.get(index, index)[0]
 
 
 ENCODER = Encoder('./utils/resources/phrases.json')
