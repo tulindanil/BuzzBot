50f6279156def21cc1b2dd202b7b4709d2996c7c
tulindanil <tulindanil@gmail.com>
2016-05-13 21:28:02 +0300

    add new public method and import Database

--- worker/worker.py
+++ worker/worker.py
@@ -1,4 +1,9 @@
+from . import Database
 
 class Worker:
+
+    def start_dialog(self, user_id):
+        return '{user_comeback}' 
+
     def keep_dialog(self, user_id, message):
         return '{no_definition}'
