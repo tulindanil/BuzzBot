1dd3703bb9d383d93db9b37d17f91faafa857a68
tulindanil <tulindanil@gmail.com>
2016-06-29 16:42:33 +0300

    proper imports, global config paths and decorators

--- worker/storage.py
+++ worker/storage.py
@@ -1,5 +1,32 @@
-import sqlite3
+import shelve
+
+from os import makedirs
+from os import path
+
+from os.path import expanduser
+from os.path import join
+from os.path import exists
+
+STORAGE_DIR = expanduser('~/.buzz_bot/')
+STORAGE_PATH = join(STORAGE_DIR, 'persistance')
 
 class Storage:
+    def folderize(f):
+        def wrapper(self, *args, **kwargs):
+            if not exists(STORAGE_DIR):
+                makedirs(STORAGE_DIR)
+            return f(self, *args, **kwargs)
+        return wrapper
 
-    def __init__(self): pass
+    @folderize
+    def synchronize(f):
+        def wrapper(self, *args, **kwargs):
+            with shelve.open(STORAGE_PATH) as db:
+                value = f(self, *args, db, **kwargs)
+            return value
+        return wrapper
+
+    @synchronize
+    def is_it_new_user(self, user_id, db):
+        # do something with db
+        return False
