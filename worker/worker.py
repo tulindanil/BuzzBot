408ad75f792c492d728a9f95a7dd28e3215e2745
tulindanil <tulindanil@gmail.com>
2016-06-29 17:34:27 +0300

    initial filling

--- worker/worker.py
+++ worker/worker.py
@@ -1,11 +1,44 @@
 from . import Storage
 
-class Worker:
+class Node():
+    def __init__():
+        self.map = {}
 
-    def __init__(self): pass
+    def add_neighbor(to_node, cmd, feedback):
+        pass
+
+class Graph():
+    def __init__(self):
+        node_names = ['napping', 'idle',
+                      'activity', 'decision']
+
+        edges = [(0, 1, '/wake', '{wake}'),
+                 (1, 0, '/snooze', '{snooze}'),
+                 (1, 3, '/do', '{decision}'),
+                 (3, 1, '/cancel', '{no_decision}'),
+                 (3, 2, '', '{activity}'),
+                 (2, 1, '/done', '{activity_is_done}')]
+
+        self.nodes = {}
+        for name in node_names:
+            self.nodes[name] = Node()
+
+        for from_idx, to_idx, cmd, feedback in edges:
+            from_node = self.nodes[from_idx]
+            to_node = self.nodes[to_idx]
+            node.add_neighbor(to_node, cmd, feedback)
+
+        self.current_node = self.nodes[node_names[1]]
+
+class Worker:
+    def __init__(self):
+        self.storage = Storage()
 
     def start_dialog(self, user_id):
-        return ['{user_comeback}']
+        if self.storage.obtain_new_user(user_id):
+            return ['{new_user}']
+        else:
+            return ['{user_comeback}']
 
     def keep_dialog(self, user_id, message):
         return ['{no_definition}']
