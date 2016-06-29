37bb16e785e36d5aee7495d6dd9f0e23829e5f0e
tulindanil <tulindanil@gmail.com>
2016-06-29 10:03:05 +0300

    wrap feedback into array, method parameteres fix

--- worker/worker.py
+++ worker/worker.py
@@ -2,10 +2,10 @@ from . import Storage
 
 class Worker:
 
-    def __init__(): pass
+    def __init__(self): pass
 
     def start_dialog(self, user_id):
-        return '{user_comeback}' 
+        return ['{user_comeback}']
 
     def keep_dialog(self, user_id, message):
-        return '{no_definition}'
+        return ['{no_definition}']
